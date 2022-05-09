865a606ab1fcd1d9c1c6dbaa40683afcad2edb62
Brandon Wright <psywb7@nottingham.ac.uk>
2022-05-09 13:58:49 +0100

    multiplication to a matrix via a vector

--- Matrix.py
+++ Matrix.py
@@ -2,5 +2,31 @@
 
 class Matrix:
 
-    def __init__(self):
-        print("Hello world")
+    def __init__(self,xdimensions,ydimensions,*numbers):
+    
+        self.arr = []
+        f = 0 
+        for i in range(ydimensions):
+            self.arr.append([])
+            for y in range(xdimensions):
+                self.arr[i].append(numbers[f])
+                f += 1
+        
+    
+    def printMatrix(self):
+        for i in range(len(self.arr)):
+            print(self.arr[i])
+
+    def vectorsToMatrix(self):
+        pass
+
+    def timesByVector(self,Vector):
+        for i in range(len(self.arr)):
+            for y in range(len(Vector.vec)):
+                self.arr[i][y] *= Vector.vec[y]
+        
+            
+            
+
+        
+
--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 ## for testing purposes
 
 from Matrix import *
+from Vector import *
 
-m = Matrix()
+m = Matrix(2,2,3,2,-2,1)
+v = Vector(1,2)
 
+m.timesByVector(v)
+
+m.printMatrix()
 
 ## LINEAR ALG VIDEO 4
